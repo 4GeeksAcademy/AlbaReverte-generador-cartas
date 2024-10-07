3363840955709bbf5d017686a32760f8bde0848b
AlbaReverte <albareverteruiz@gmail.com>
2024-10-07 19:03:28 +0000

    Generador cartas

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+/*import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";*/
+import { suit, values } from "./card";
+
+const showCard = (ramdomSuit, ramdomValues) => {
+  const card = document.querySelector("#card");
+  card.innerHTML = `
+    <div>${ramdomSuit}</div>
+    <div>${ramdomValues}</div>
+    <div>${ramdomSuit}</div>`;
+};
+
+const generateRandomCard = () => {
+  const ramdomSuit = suit[Math.floor(Math.random() * 4)];
+  const ramdomValues = values[Math.floor(Math.random() * 13)];
+  showCard(ramdomSuit, ramdomValues);
+};
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  generateRandomCard();
+
+  document
+    .querySelector("#btn-card")
+    .addEventListener("click", () => generateRandomCard());
 };
---
+++ src/card.js
@@ -0,0 +1,17 @@
+export const suit = ["♥", "♠", "♣", "♦ "];
+
+export const values = [
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  "Rey",
+  "Reina",
+  "Jota",
+  "As"
+];
